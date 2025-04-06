ab564eb7ad61f5ff2557879cb1479995a5c9a44f
Muiliyu Sodiq Akintomide <msodiqakintomide@gmail.com>
2025-04-06 13:47:03 +0100

    fixed errors on the Cors and Env

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN npm install
 
 COPY . .
 
-# RUN node ace build
+RUN node ace build
 
 EXPOSE 3333
 
--- config/cors.ts
+++ config/cors.ts
@@ -1,10 +1,5 @@
 
 
-import type { CorsConfig } from '@ioc:Adonis/Core/Cors'
-
-import Env from '@ioc:Adonis/Core/Env'
-
-
 const corsConfig = {
   enabled: true,
   origin: ['http://localhost:5173'],
